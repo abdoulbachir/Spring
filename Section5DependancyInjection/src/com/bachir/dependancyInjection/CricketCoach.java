dd65c13784fbd7758b45c77bdf9877072a79f6d0
zabdoulbachir@gmail.com <zabdoulbachir@gmail.com>
2023-02-07 22:26:06 -0600

    new updates. Section 5 finished!

--- Section5DependancyInjection/src/com/bachir/dependancyInjection/CricketCoach.java
+++ Section5DependancyInjection/src/com/bachir/dependancyInjection/CricketCoach.java
@@ -3,6 +3,8 @@ package com.bachir.dependancyInjection;
 public class CricketCoach implements Coach {
 	
 	private FortuneService fortuneService;
+	private String emailAddress;
+	private String team;
 	
 
 	//No-arg constructor
@@ -16,6 +18,24 @@ public class CricketCoach implements Coach {
 		this.fortuneService = theFortuneService;
 	}
 
+	//setter for the new variables
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("CricketCoach: inside setter method - setEmailAddress");
+		this.emailAddress = emailAddress;
+	}
+	public void setTeam(String team) {
+		System.out.println("CricketCoach: inside setter method - setTeam");
+		this.team = team;
+	}
+	
+	
+	//getter for the new variables
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	public String getTeam() {
+		return team;
+	}
 	@Override
 	public String getDailyWorkOut() {
 		return "Practice fast bowling for 15 minutes.";
